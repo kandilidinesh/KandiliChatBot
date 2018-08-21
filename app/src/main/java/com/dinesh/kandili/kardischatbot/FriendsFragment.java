c7330c2b58ea225703501599bd0227b0583af833
kandilidinesh <kandilidinesh@gmail.com>
2018-08-21 16:37:49 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/dinesh/kandili/kardischatbot/FriendsFragment.java
+++ app/src/main/java/com/dinesh/kandili/kardischatbot/FriendsFragment.java
@@ -28,6 +28,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -69,8 +71,6 @@ public class FriendsFragment extends Fragment {
         mFriendsDatabase.keepSynced(true);
         mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
         mUsersDatabase.keepSynced(true);
-
-
         mFriendsList.setHasFixedSize(true);
         mFriendsList.setLayoutManager(new LinearLayoutManager(getContext()));
 
@@ -93,9 +93,27 @@ public class FriendsFragment extends Fragment {
 
         ) {
             @Override
-            protected void populateViewHolder(final FriendsViewHolder friendsViewHolder, Friends friends, int i) {
+            protected void populateViewHolder(final FriendsViewHolder friendsViewHolder, final Friends friends, int i) {
 
                 friendsViewHolder.setDate(friends.getDate());
+
+                String list_user_id = getRef(i).getKey();
+
+                mUsersDatabase.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String userName = dataSnapshot.child("name").getValue().toString();
+                        String image = dataSnapshot.child("image").getValue().toString();
+
+                        friendsViewHolder.setName(userName);
+                        friendsViewHolder.setImage(image);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
         };
         mFriendsList.setAdapter(friendsRecyclerViewAdapter);
@@ -124,6 +142,28 @@ public class FriendsFragment extends Fragment {
             TextView userNameView = (TextView) mView.findViewById(R.id.users_single_name);
             userNameView.setText(name);
 
+        }
+        public void setImage(final String image)
+        {
+            final CircleImageView circleImageView = (CircleImageView) mView.findViewById(R.id.user_single_image);
+
+            if(!image.equals("default"))
+            {
+//                Picasso.get().load(image).placeholder(R.drawable.unknown).into(circleImageView);
+                Picasso.get().load(image)
+                        .networkPolicy(NetworkPolicy.OFFLINE)
+                        .placeholder(R.drawable.unknown).into(circleImageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Picasso.get().load(image).placeholder(R.drawable.unknown).into(circleImageView);
+                    }
+                });
+            }
         }
 
     }
